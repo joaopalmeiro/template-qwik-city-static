6c70ad0dec8214966722f5d7b3a56cc3ff6bd112
João Palmeiro <joaommpalmeiro@gmail.com>
2024-02-16 15:45:14 +0000

    Closes #4

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
@@ -7,3 +8,13 @@ export default component$(() => {
     </>
   );
 });
+
+export const head: DocumentHead = {
+  title: "template-qwik-city-static",
+  meta: [
+    {
+      name: "description",
+      content: "Opinionated Qwik City + SSG template for new projects.",
+    },
+  ],
+};
